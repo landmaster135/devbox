87fccbd52ddea56d994a2900c8a5a1d2881842fa
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2025-04-28 10:16:27 +0900

    Add features to convert iso8601 to unix and reversely into json_timestamp_modifier

---
+++ pkg/dos/Z6-3_json_modifier_to_get.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set /p filepath="Input path of JSON file you wanna edit: "
+set /p key="Input key to get: "
+choice /c yn /n /m "Select whether to get all keys and values or not  [y]='-get-all'  [n]='-get' : "
+if errorlevel 1 (
+  set "gets_all=-get-all"
+) else (
+  set "gets_all=-get"
+)
+
+echo --- プログラムを実行します ---
+.\pkg\bin\win_amd64\json-timestamp-modifier.exe -file %filepath% -key %key% %gets_all%
+echo.
+echo --- プログラムの実行が完了しました ---
+echo --- 何かキーを押すと終了します ---
+pause > nul
