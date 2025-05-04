1380478a0d3df30f28de8ed107389afed1e8f1bd
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2025-05-05 02:22:44 +0900

    Update DOS file to convert to jpeg

--- pkg/dos/Z3-1_image_converter_to_jpg.bat
+++ pkg/dos/Z3-1_image_converter_to_jpg.bat
@@ -1,5 +1,6 @@
 @echo off
 
+set /p quality="Input integer (75 as default) for quality of output jpg (1-100): "
 choice /c yn /n /m "Select whether to move original image files to archive directory or not  [y]='--move'  [n]='' : "
 if %errorlevel% == 1 (
   set "moves=-move"
@@ -9,7 +10,7 @@ if %errorlevel% == 1 (
 echo %moves%
 
 echo --- プログラムを実行します ---
-.\pkg\bin\win_amd64\image-converter.exe -src . -ext jpg -q 75 -archive .\5_original_files %moves%
+.\pkg\bin\win_amd64\image-converter.exe -src . -ext jpg -q %quality% -archive .\5_original_files %moves%
 echo.
 echo --- プログラムの実行が完了しました ---
 echo --- 何かキーを押すと終了します ---
