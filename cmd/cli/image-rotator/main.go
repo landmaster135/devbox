58c2ccd1a81f4843bd350a28864fc75f95d44e24
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2025-05-05 16:53:53 +0900

    refactor: Move image processing functions to usecases package

--- cmd/cli/image-rotator/main.go
+++ cmd/cli/image-rotator/main.go
@@ -7,11 +7,9 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
-	"strings"
 	"sync"
 
-	"github.com/anthonynsimon/bild/imgio"
-	"github.com/anthonynsimon/bild/transform"
+	"github.com/landmaster135/devbox/internal/independencies/image_rotator/usecases"
 )
 
 // exitCode は OS に返す終了ステータス
@@ -60,7 +58,7 @@ func run(args []string, stdout, stderr io.Writer) exitCode {
 			if err != nil || d.IsDir() {
 				return nil
 			}
-			if isImage(path) {
+			if usecases.IsImage(path) {
 				paths <- path
 			}
 			return nil
@@ -74,7 +72,7 @@ func run(args []string, stdout, stderr io.Writer) exitCode {
 				return
 			}
 			for _, e := range entries {
-				if !e.IsDir() && isImage(e.Name()) {
+				if !e.IsDir() && usecases.IsImage(e.Name()) {
 					paths <- filepath.Join(*srcDir, e.Name())
 				}
 			}
@@ -91,7 +89,7 @@ func run(args []string, stdout, stderr io.Writer) exitCode {
 		go func() {
 			defer wg.Done()
 			for p := range paths {
-				if err := rotateAndSave(p, *outDir, *suffix, *angleDeg); err != nil {
+				if err := usecases.RotateAndSave(p, *outDir, *suffix, *angleDeg); err != nil {
 					mu.Lock()
 					failed++
 					mu.Unlock()
@@ -99,7 +97,7 @@ func run(args []string, stdout, stderr io.Writer) exitCode {
 					continue
 				}
 				if *moveOrig {
-					if err := moveFile(p, *arcDir); err != nil {
+					if err := usecases.MoveFile(p, *arcDir); err != nil {
 						fmt.Fprintf(stderr, "アーカイブ失敗 (%s): %v\n", p, err)
 					}
 				}
@@ -118,68 +116,4 @@ func run(args []string, stdout, stderr io.Writer) exitCode {
 		return exitErr
 	}
 	return exitOK
-}
-
-// ---------------------------------------------------------
-// 画像判定（拡張子）
-// ---------------------------------------------------------
-func isImage(path string) bool {
-	switch strings.ToLower(filepath.Ext(path)) {
-	case ".jpg", ".jpeg", ".png":
-		return true
-	default:
-		return false
-	}
-}
-
-// ---------------------------------------------------------
-// actual rotate & save
-// ---------------------------------------------------------
-func rotateAndSave(srcPath, outDir, suffix string, angle float64) error {
-	img, err := imgio.Open(srcPath)
-	if err != nil {
-		return fmt.Errorf("open: %w", err)
-	}
-
-	rot := transform.Rotate(img, angle, &transform.RotationOptions{
-		ResizeBounds: true, // 回転後に画像全体が入るキャンバスへ拡大
-		// Pivot は nil なら自動で中心
-	})
-
-	ext := strings.ToLower(filepath.Ext(srcPath))
-	name := strings.TrimSuffix(filepath.Base(srcPath), ext)
-	outPath := filepath.Join(outDir, fmt.Sprintf("%s_%s%s", name, suffix, ext))
-
-	if err := ensureDir(outDir); err != nil {
-		return err
-	}
-
-	var enc imgio.Encoder
-	switch ext {
-	case ".jpg", ".jpeg":
-		enc = imgio.JPEGEncoder(95) // 品質 95 でエンコーダー作成
-	case ".png":
-		enc = imgio.PNGEncoder()
-	}
-
-	if err := imgio.Save(outPath, rot, enc); err != nil {
-		return fmt.Errorf("save: %w", err)
-	}
-	return nil
-}
-
-// ---------------------------------------------------------
-// originals をアーカイブへ移動
-// ---------------------------------------------------------
-func moveFile(src, dstDir string) error {
-	if err := ensureDir(dstDir); err != nil {
-		return err
-	}
-	dst := filepath.Join(dstDir, filepath.Base(src))
-	return os.Rename(src, dst)
-}
-
-// dstDir が無ければ作成
-func ensureDir(dir string) error {
-	return os.MkdirAll(dir, 0o755)
 }
--- internal/independencies/image_rotator/usecases/services.go
+++ internal/independencies/image_rotator/usecases/services.go
@@ -1,1 +1,75 @@
 package usecases
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/anthonynsimon/bild/imgio"
+	"github.com/anthonynsimon/bild/transform"
+)
+
+// ---------------------------------------------------------
+// 画像判定（拡張子）
+// ---------------------------------------------------------
+func IsImage(path string) bool {
+	switch strings.ToLower(filepath.Ext(path)) {
+	case ".jpg", ".jpeg", ".png":
+		return true
+	default:
+		return false
+	}
+}
+
+// ---------------------------------------------------------
+// actual rotate & save
+// ---------------------------------------------------------
+func RotateAndSave(srcPath, outDir, suffix string, angle float64) error {
+	img, err := imgio.Open(srcPath)
+	if err != nil {
+		return fmt.Errorf("open: %w", err)
+	}
+
+	rot := transform.Rotate(img, angle, &transform.RotationOptions{
+		ResizeBounds: true, // 回転後に画像全体が入るキャンバスへ拡大
+		// Pivot は nil なら自動で中心
+	})
+
+	ext := strings.ToLower(filepath.Ext(srcPath))
+	name := strings.TrimSuffix(filepath.Base(srcPath), ext)
+	outPath := filepath.Join(outDir, fmt.Sprintf("%s_%s%s", name, suffix, ext))
+
+	if err := ensureDir(outDir); err != nil {
+		return err
+	}
+
+	var enc imgio.Encoder
+	switch ext {
+	case ".jpg", ".jpeg":
+		enc = imgio.JPEGEncoder(95) // 品質 95 でエンコーダー作成
+	case ".png":
+		enc = imgio.PNGEncoder()
+	}
+
+	if err := imgio.Save(outPath, rot, enc); err != nil {
+		return fmt.Errorf("save: %w", err)
+	}
+	return nil
+}
+
+// ---------------------------------------------------------
+// originals をアーカイブへ移動
+// ---------------------------------------------------------
+func MoveFile(src, dstDir string) error {
+	if err := ensureDir(dstDir); err != nil {
+		return err
+	}
+	dst := filepath.Join(dstDir, filepath.Base(src))
+	return os.Rename(src, dst)
+}
+
+// dstDir が無ければ作成
+func ensureDir(dir string) error {
+	return os.MkdirAll(dir, 0o755)
+}
