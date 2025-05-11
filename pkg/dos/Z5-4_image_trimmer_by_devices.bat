ba43deb37343e44cd3585b14c477d038b860ddfc
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2025-05-12 01:05:09 +0900

    feat: Add batch scripts for image trimming, JSON file merging, and API client interactions

---
+++ pkg/dos/Z5-4_image_trimmer_by_devices.bat
@@ -0,0 +1,29 @@
+@echo off
+
+set coordinates="-x1 0 -y1 131 -x2 1080 -y2 2293"
+choice /c a /n /m "Select how to trim images by devices taking screenshots  [a]='Android' : "
+if %errorlevel% == 1 (
+  set "coordinate_x1=0"
+  set "coordinate_y1=131"
+  set "coordinate_x2=1080"
+  set "coordinate_y2=2293"
+) else (
+  set "coordinate_x1="
+  set "coordinate_y1="
+  set "coordinate_x2="
+  set "coordinate_y2="
+)
+choice /c yn /n /m "Select whether to move original image files to archive directory or not  [y]='--move'  [n]='' : "
+if %errorlevel% == 1 (
+  set "moves=-move"
+) else (
+  set "moves="
+)
+echo %moves%
+
+echo --- プログラムを実行します ---
+.\pkg\bin\win_amd64\image-trimmer.exe -src . -suffix cropped -x1 %coordinate_x1% -y1 %coordinate_y1% -x2 %coordinate_x2% -y2 %coordinate_y2% %moves%
+echo.
+echo --- プログラムの実行が完了しました ---
+echo --- 何かキーを押すと終了します ---
+pause > nul
---
+++ pkg/dos/Z6-4_json_file_merger_for_pc_stats.bat
@@ -0,0 +1,10 @@
+@echo off
+set dirpath=".\9_json"
+set key="pc_stats"
+set output=".\9_json\__merged_%date:~,4%%date:~5,2%%date:~8,2%.json"
+echo --- プログラムを実行します ---
+.\pkg\bin\win_amd64\json-file-merger.exe -dir %dirpath% -key %key% -output %output%
+echo.
+echo --- プログラムの実行が完了しました ---
+echo --- 何かキーを押すと終了します ---
+pause > nul
