bcb477fbb4490e67f5ac83fc32e9a00cd3442a26
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2025-05-18 10:16:18 +0900

    feat: Fix command syntax in blur script and add new script for Genshin captures

--- pkg/dos/Z5-7_image_filterer_on_blur_for_genshin_captures.bat
+++ pkg/dos/Z5-7_image_filterer_on_blur_for_genshin_captures.bat
@@ -1,6 +1,5 @@
 @echo off
 
-set /p coordinates="Input 4 coordinates linked on each parameter to trim images (-x1, -y1, -x2, -y2): "
 choice /c yn /n /m "Select whether to move original image files to archive directory or not  [y]='--move'  [n]='' : "
 if %errorlevel% == 1 (
   set "moves=-move"
@@ -10,7 +9,7 @@ if %errorlevel% == 1 (
 echo %moves%
 
 echo --- プログラムを実行します ---
-.\pkg\bin\win_amd64\image-filterer.exe -src . -suffix blurred %coordinates% %moves% -mode blur -radius 50
+.\pkg\bin\win_amd64\image-filterer.exe -src . -suffix blurred -x1 1690 -y1 1055 -x2 1872 -y2 1080 %moves% -mode blur -radius 50
 echo.
 echo --- プログラムの実行が完了しました ---
 echo --- 何かキーを押すと終了します ---
