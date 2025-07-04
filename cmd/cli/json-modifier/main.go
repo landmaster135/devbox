11b6a2fb41ec3d7d524067820a6625e6fe8a66c9
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2025-07-05 03:46:04 +0900

    refactor: Implement JSON service for manipulating JSON files into independencies package

--- cmd/cli/json-modifier/main.go
+++ cmd/cli/json-modifier/main.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/landmaster135/devbox/internal/interfaces/repositories"
-	"github.com/landmaster135/devbox/internal/usecases/services"
+	"github.com/landmaster135/devbox/internal/independencies/json_modifier/interfaces/repositories"
+	"github.com/landmaster135/devbox/internal/independencies/json_modifier/usecases/services"
 )
 
 // exitCode はプログラムの終了コードを表します
