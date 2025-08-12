c1ea35acd4921e6a98542b0c8498345458e6f764
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2025-08-13 05:44:52 +0900

    feat: add dos files for image trimming functionality with user-defined coordinates and quality settings

--- pkg/dos/Z5-2_pdf_merger_with_trimming_images.bat
+++ pkg/dos/Z5-2_pdf_merger_with_trimming_images.bat
@@ -16,17 +16,12 @@ if /i "%purpose%"=="g" (
   pause > nul
   exit /b 1
 )
-
-choice /c yn /n /m "Select whether to move original image files to archive directory or not  [y]='--move'  [n]='' : "
-if %errorlevel% == 1 (
-  set "moves=-move"
-) else (
-  set "moves="
-)
-echo %moves%
+set /p quality="Input integer (75 as default, 30 useful for comic) for quality of output jpg (1-100): "
 
 echo --- プログラムを実行します ---
-.\pkg\bin\cli\win_amd64\image-trimmer.exe -src . -suffix cropped -x1 %coordinate_x1% -y1 %coordinate_y1% -x2 %coordinate_x2% -y2 %coordinate_y2% %moves%
+.\pkg\bin\cli\win_amd64\image-trimmer.exe -src . -suffix cropped -x1 %coordinate_x1% -y1 %coordinate_y1% -x2 %coordinate_x2% -y2 %coordinate_y2% -move
+.\pkg\bin\cli\win_amd64\image-converter.exe -src . -ext jpg -q %quality% -archive .\5_original_files -move
+.\pkg\bin\cli\win_amd64\pdf-merger.exe -dir %HOMEPATH%\Downloads\picture_backup
 echo.
 echo --- プログラムの実行が完了しました ---
 echo --- 何かキーを押すと終了します ---
