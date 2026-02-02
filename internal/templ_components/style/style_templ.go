42e048fbd5069329d18fbc8eb3c14e1a95c6c3cd
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2026-02-03 00:02:41 +0900

    refactor: replace inline CSS with structured style component system and extract cron workflow styles

--- internal/templ_components/style/style_templ.go
+++ internal/templ_components/style/style_templ.go
@@ -8,7 +8,7 @@ package style
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-func Tag(content string) templ.Component {
+func Tag(s CSSStyle) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
@@ -29,15 +29,7 @@ func Tag(content string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templ.Raw("<style>").Render(ctx, templ_7745c5c3_Buffer)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		templ_7745c5c3_Err = templ.Raw(content).Render(ctx, templ_7745c5c3_Buffer)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		templ_7745c5c3_Err = templ.Raw("</style>").Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = renderStyle(s).Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
